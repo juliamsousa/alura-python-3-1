d7f1402ac25bb648b018e5102d0d909bb398e872
Júlia Miranda <juliaeduardasousa@gmail.com>
2021-06-17 01:34:14 -0300

    Loop reestructuring whith FOR and RANGE

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,9 +4,16 @@ print ("=================================")
 
 secret_number = 42
 total_attempts = 10
+round = 1
 
-while (total_attempts > 0):
-    print("Tentativas restantes", total_attempts)
+# for e um laco alternativo ao while e implementa o incremento de forma mais facil
+# for <variavel> in range (a, b, step):
+# for <variavel> in range [1, 2, 3, ..., n]:
+# o conjunto eh (a, b], ou seja o limite superior nao esta incluso
+
+for round in range (1, total_attempts+1):
+    # utilizamos a funcao format (f) que aplica a string interpolation
+    print(f"Tentativa {round} de {total_attempts}")
     # convertemos a input para int, pois em alguns casos vem com tipo <str>
     guess = int(input("Digite o seu numero: "))
 
@@ -29,7 +36,5 @@ while (total_attempts > 0):
         # nesse caso poderia ser resolvido com um if, mas e pra carater didatico
         elif (smaller_than):
             print ("Errooooou! Pode subir")
-
-    total_attempts = total_attempts - 1
 
 print ("Fim de jogo!")
