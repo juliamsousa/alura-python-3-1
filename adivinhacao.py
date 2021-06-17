62a1981f826ced23b143d7a95987b3c1726cc063
Júlia Miranda <juliaeduardasousa@gmail.com>
2021-06-17 00:38:44 -0300

    Testin if and else

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,15 @@
+print ("=================================")
 print ("Bem vindo ao jogo de adivinhacao!")
+print ("=================================")
+
+secret_number = 42
+
+# convertemos a input para int, pois em alguns casos vem com tipo <str>
+guess = int(input("Digite o seu numero: "))
+
+print("Voce digitou ", guess)
+
+if (guess == secret_number):
+    print ("Acertou miseravi!")
+else:
+    print ("Errooooou!")
