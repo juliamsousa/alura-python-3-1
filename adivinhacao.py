4210e5ba5f256f6b1991f3a3ba545a03db26a4a1
Júlia Miranda <juliaeduardasousa@gmail.com>
2021-06-17 01:01:51 -0300

    Improving code legibility

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,9 +7,23 @@ secret_number = 42
 # convertemos a input para int, pois em alguns casos vem com tipo <str>
 guess = int(input("Digite o seu numero: "))
 
-print("Voce digitou ", guess)
+# convertemos guess para <str> para melhorar a visualizacao do texto
+# com isso podemos concatenar as strings com o operador +
+print("Voce digitou", guess)
 
-if (guess == secret_number):
+# tornando o codigo mais legivel
+guessed = guess == secret_number
+bigger_than = guess > secret_number
+smaller_than = guess < secret_number
+
+# desse modo temos apenas um chute
+if (guessed):
     print ("Acertou miseravi!")
 else:
-    print ("Errooooou!")
+    if (bigger_than):
+        print ("Errooooou! Chutou na lua")
+    # com o elif caso o teste acima seja verdade, nao e testado
+    # nesse caso poderia ser resolvido com um if, mas e pra carater didatico
+    elif (smaller_than):
+        print ("Errooooou! Pode subir")
+print ("Fim de jogo!")
