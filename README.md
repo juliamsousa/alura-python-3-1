a75722e8eace0064036abea77d476361fe7b67c2
Júlia Miranda <juliaeduardasousa@gmail.com>
2021-06-21 22:53:29 -0300

    WIP - Loop and limits

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Repositório contendo código desenvolvido ao longo do curso Python 3 parte 1: I
 
 Mudando versão do Python no projeto do PyCharm: https://www.jetbrains.com/help/pycharm/configuring-python-interpreter.html#add-existing-interpreter
 
+https://docs.python.org/3/library/string.html#formatexamples
 
+https://pyformat.info/
 
-No link abaixo, você encontra o projeto até o momento atual do curso: https://github.com/alura-cursos/Curso-Python-3-parte-1-Introdu-o-nova-vers-o-da-linguagem/archive/capitulo2.zip
 
--- adivinhacao.py
+++ adivinhacao.py
@@ -5,6 +5,8 @@ print ("=================================")
 secret_number = 42
 total_attempts = 10
 round = 1
+inferior_limit = 1
+superior_limit = 100
 
 # for e um laco alternativo ao while e implementa o incremento de forma mais facil
 # for <variavel> in range (a, b, step):
@@ -14,12 +16,16 @@ round = 1
 for round in range (1, total_attempts+1):
     # utilizamos a funcao format (f) que aplica a string interpolation
     print(f"Tentativa {round} de {total_attempts}")
+
     # convertemos a input para int, pois em alguns casos vem com tipo <str>
-    guess = int(input("Digite o seu numero: "))
+    guess = int(input(f"Digite um número entre {inferior_limit} e {superior_limit}: "))
+    print("Voce digitou", guess)
 
     # convertemos guess para <str> para melhorar a visualizacao do texto
     # com isso podemos concatenar as strings com o operador +
-    print("Voce digitou", guess)
+    if (guess < inferior_limit or guess > superior_limit):
+        print(f"Você deve digitar um número dentro do intervalo {inferior_limit} : {superior_limit}")
+        continue
 
     # tornando o codigo mais legivel
     guessed = guess == secret_number
@@ -29,6 +35,7 @@ for round in range (1, total_attempts+1):
     # desse modo temos apenas um chute
     if (guessed):
         print ("Acertou miseravi!")
+        break
     else:
         if (bigger_than):
             print ("Errooooou! Chutou na lua")
